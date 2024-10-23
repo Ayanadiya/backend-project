8ac2362f7d1a2a62ca1f9838c05c716a13ecf741
Ayanadiya <ayanadiya17@gmail.com>
2024-10-23 14:56:46 +0530

    cart model created

---
+++ models/cart.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'cart.json'
+);
+
+module.exports= class Cart {
+   static addproduct(id,productprice) {
+         //fetch the previous cart
+        fs.readFile(p, (err,fileContent)=>{
+            let cart={products:[], totalprice:0};
+            if(!err){
+                cart=JSON.parse(fileContent);
+            }
+            //analyze the cart -find existing products
+            const existingproductIndex=cart.products.findIndex(prod => prod.id=id);
+            const existingproduct=cart[existingproductIndex];
+            let updatedproduct;
+            // add new product , increase quantity and price
+            if(existingproduct){
+                updatedproduct={...existingproduct};
+                updatedproduct.qty=updatedproduct.qty+1;
+                cart.products=[...cart.products];
+                cart.products[existingproductIndex]=updatedproduct;
+            }
+            else
+            {
+                updatedproduct= { id:id, qty:1};
+                cart.products=[...cart.products,updatedproduct];
+            }
+            cart.totalprice=cart.totalprice + +productprice;
+            fs.writeFile(p, JSON.stringify(cart),err =>{
+                console.log(err);
+            })
+        })
+   }
+}
