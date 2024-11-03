a161a6029d1f449ad2e9572abdeb1690e01c1550
Ayanadiya <ayanadiya17@gmail.com>
2024-11-03 11:35:16 +0530

    learned to set up relationships in sequelize

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const sequelize=require('./util/database');
+const Product=require('./models/product');
+const User=require('./models/user');
 
 const app = express();
 
@@ -19,12 +21,34 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    User.findByPk(1)
+    .then(user => {
+        req.user=user;
+        next();
+    })
+    .catch(err => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, {constraints:true, onDelete:'CASCADE'});
+User.hasMany(Product);
+
 sequelize.sync()
+.then(result => {
+    return User.findByPk(1);
+})
+.then(user => {
+    if(!user)
+    {
+        return User.create({name:'ayana', email:'ayana@gmail.com'})
+    }
+    return user;
+})
 .then(result =>{
     console.log(result);
     app.listen(3000);
--- controllers/admin.js
+++ controllers/admin.js
@@ -16,12 +16,14 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
+  req.user
+  .createProduct({
     title: title,
     price: price,
     imageUrl: imageUrl,
-    description: description
-  }).then(result => {
+    description: description,
+  })
+  .then(result => {
     console.log("product created");
     res.redirect('/admin/products');
 })
@@ -35,8 +37,10 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/')
   }
   const prodId=req.params.productId;
-  Product.findByPk(prodId)
-  .then(product => {
+  req.user
+  .getProducts({where:{id:prodId}})
+  .then(products => {
+    const product = products[0];
     if(!product)
       {
         return res.redirect('/');
@@ -51,7 +55,8 @@ exports.getEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user
+  .getProducts()
   .then(products => {
     res.render('admin/products', {
       prods: products,
